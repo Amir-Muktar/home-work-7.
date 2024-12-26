d3a0efb46253817bb0cb5fdc9f751a07b96a9de8
Amir-Muktar <erkingulbegalieva4@gmail.com>
2024-12-27 01:50:48 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import TodosPage from './pages/TodosPage/TodosPage';
+
+
+
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <TodosPage/>
+
+
+        </div>
+    );
+}
+
+export default App;
